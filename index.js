3f9ae2cdeca1d95883e7bf7c5166edd4558bc075
Jose Lopez <jooooeyla@outlook.com>
2017-09-25 23:51:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
 logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase()===string) {
     return "I can't hear you!"
@@ -21,5 +24,5 @@ function sayHiToGrandma(string) {
   if (string==="Ilove you, Grandma") {
     return "I love you, too."
   }
-  return "Are you eating enough?"
+   return "Are you eating enough?"
 }
